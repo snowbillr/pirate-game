f0d8a7a2df82f6260c073f7b426c89cab6c3eac5
Bill Reed <billnreed@gmail.com>
2018-03-22 13:34:31 -0400

    fix indentation

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,37 @@
 class MyScene extends Phaser.Scene {
   preload() {
-      this.load.image('logo', 'assets/logo.png');
+    this.load.image('logo', 'assets/logo.png');
   }
 
   create() {
-      var logo = this.add.image(10, 10, 'logo');
+    const logo = this.add.image(50, 50, 'logo');
+    const logo2 = this.add.image(150, 50, 'logo');
 
-      this.tweens.add({
-          targets: logo,
-          x: 450,
-          duration: 2000,
-          ease: 'Power2',
-          yoyo: true,
-          loop: -1
-      });
+    this.tweens.add({
+      targets: logo,
+      duration: 2000,
+      ease: 'Power2',
+      loop: -1,
+
+      rotation: Math.PI,
+    });
+
+    this.tweens.add({
+      targets: logo2,
+      duration: 2000,
+      ease: 'Power2',
+      loop: -1,
+
+      rotation: -Math.PI,
+    });
   }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: [MyScene]
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: [MyScene]
 };
 
 new Phaser.Game(config);
-
