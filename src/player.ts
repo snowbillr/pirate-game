9086f97e90c26f961780e42d7962bb0a391b41d1
Bill Reed <billnreed@gmail.com>
2018-04-01 23:21:34 -0400

    player movement state machine

--- src/player.ts
+++ src/player.ts
@@ -3,11 +3,59 @@ export class Player {
   public sprite; //: Phaser.GameObjects.Sprite;
   private controls; //: { [string]: Phaser.Input.Keyboard.Key }
 
+  private movementState; //: PlayerMovementState
+  private idleState;
+  private walkingState;
+
+  private direction;
+
   constructor(scene) {
     this.scene = scene;
 
     this.sprite = null;
     this.controls = null;
+    this.movementState = null;
+
+    this.idleState = {
+      key: 'idle',
+      onEnter: () => {
+        this.sprite.setFrame('adventurer_stand.png');
+      },
+      onUpdate: () => {
+        if (this.controls.left.isDown || this.controls.right.isDown) {
+          this.transition(this.walkingState);
+        }
+      },
+      onLeave: () => {
+
+      }
+    }
+    this.walkingState = {
+      key: 'walking',
+      onEnter: () => {
+        this.sprite.play('player_walk');
+
+        if (this.controls.left.isDown) {
+          this.sprite.flipX = true;
+          this.direction = Phaser.LEFT;
+        } else if (this.controls.right.isDown) {
+         this.sprite.flipX = false;
+         this.direction = Phaser.RIGHT;
+        }
+      },
+      onUpdate: () => {
+        if (this.direction === Phaser.LEFT && !this.controls.left.isDown) {
+          this.transition(this.idleState);
+        }
+        if (this.direction === Phaser.RIGHT && !this.controls.right.isDown) {
+          this.transition(this.idleState);
+        }
+      },
+      onLeave: () => {
+        this.direction = null;
+        this.sprite.anims.stop();
+      }
+    }
   }
 
   create() {
@@ -16,18 +64,20 @@ export class Player {
       'left': Phaser.Input.Keyboard.KeyCodes.LEFT,
       'right': Phaser.Input.Keyboard.KeyCodes.RIGHT,
     });
+    this.movementState = this.idleState;
   }
 
   update() {
-    if (this.controls.left.isDown) {
-      this.sprite.flipX = true;
-      this.sprite.play('player_walk', true);
-    } else if (this.controls.right.isDown) {
-      this.sprite.flipX = false;
-      this.sprite.play('player_walk', true);
-    } else {
-      this.sprite.anims.stop();
-      this.sprite.setFrame('adventurer_stand.png');
+    this.movementState.onUpdate();
+  }
+
+  private transition(to) {
+    if (this.movementState.key == to.key) {
+      return;
     }
+
+    this.movementState.onLeave();
+    this.movementState = to;
+    this.movementState.onEnter();
   }
 }
