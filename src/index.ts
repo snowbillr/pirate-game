b23fb8209949f093e419516f514c23c4ede24d01
Bill Reed <billnreed@gmail.com>
2018-04-03 22:56:58 -0400

    extract scene to its own file

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,4 @@
-import { Player } from './player/player';
-
-class MyScene extends Phaser.Scene {
-  private player: Player;
-
-  init() {
-    this.player = new Player(this);
-  }
-
-  preload() {
-    this.load.atlas('player', 'assets/sprite-atlases/player.png', 'assets/sprite-atlases/player.json');
-
-    // tilemap stuff
-    this.load.image('kenney-platformer-redux-ground', 'assets/tilesets/kenney-platformer-redux-ground.png');
-    this.load.tilemapTiledJSON('test-map', 'assets/maps/test-map.json');
-  }
-
-  create() {
-    this.anims.create({
-      key: 'player_walk',
-      frames: [
-        { key: 'player', frame: 'adventurer_walk1.png' },
-        { key: 'player', frame: 'adventurer_walk2.png' },
-      ],
-      frameRate: 6,
-      repeat: -1,
-    });
-
-    this.tilemap = this.add.tilemap('test-map', 128, 128);
-    const tileset = this.tilemap.addTilesetImage('test-tileset', 'kenney-platformer-redux-ground')
-    this.layer = this.tilemap.createStaticLayer('Tile Layer 1', tileset, 0, 0);
-
-    this.layer.setCollisionByProperty({ collides: true }, true)
-
-    this.player.create();
-    this.add.existing(this.player.sprite);
-  }
-
-  update() {
-    this.player.update();
-
-    this.physics.add.collider(this.player.sprite, this.layer);
-  }
-}
+import { TestScene } from './scenes/test-scene';
 
 const config = {
   type: Phaser.AUTO,
@@ -55,7 +12,7 @@ const config = {
       }
     },
   },
-  scene: [MyScene]
+  scene: [TestScene]
 };
 
 new Phaser.Game(config);
---
+++ src/scenes/test-scene.ts
@@ -0,0 +1,44 @@
+import { Player } from "../player/player";
+
+export class TestScene extends Phaser.Scene {
+  private player: Player;
+
+  init() {
+    this.player = new Player(this);
+  }
+
+  preload() {
+    this.load.atlas('player', 'assets/sprite-atlases/player.png', 'assets/sprite-atlases/player.json');
+
+    // tilemap stuff
+    this.load.image('kenney-platformer-redux-ground', 'assets/tilesets/kenney-platformer-redux-ground.png');
+    this.load.tilemapTiledJSON('test-map', 'assets/maps/test-map.json');
+  }
+
+  create() {
+    this.anims.create({
+      key: 'player_walk',
+      frames: [
+        { key: 'player', frame: 'adventurer_walk1.png' },
+        { key: 'player', frame: 'adventurer_walk2.png' },
+      ],
+      frameRate: 6,
+      repeat: -1,
+    });
+
+    this.tilemap = this.add.tilemap('test-map', 128, 128);
+    const tileset = this.tilemap.addTilesetImage('test-tileset', 'kenney-platformer-redux-ground')
+    this.layer = this.tilemap.createStaticLayer('Tile Layer 1', tileset, 0, 0);
+
+    this.layer.setCollisionByProperty({ collides: true }, true)
+
+    this.player.create();
+    this.add.existing(this.player.sprite);
+  }
+
+  update() {
+    this.player.update();
+
+    this.physics.add.collider(this.player.sprite, this.layer);
+  }
+}
